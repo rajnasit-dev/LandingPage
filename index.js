fafa3c9cc64e2bc6ef44a9c5321dc3106d983ae7
Raj Nasit <nasitraj5678@gmail.com>
2025-10-21 23:51:26 +0530

    Add GSAP ANIMATION

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const navLinks = document.querySelector(".nav-links");
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+
+gsap.from(".about .container", {
+  y:110,
+  opacity:0,
+  duration:1,
+  delay:1,
+})
